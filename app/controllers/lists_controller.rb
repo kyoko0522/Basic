24584aecf0d18fb4c422ab14d630364e51800657
kyoko0522 <kyo08023971867@gmail.com>
2022-06-21 07:48:23 +0000

    Initial Commit

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,52 @@
+class ListsController < ApplicationController
+
+  def index
+    @list = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+     @list = List.new(list_params)
+   if @list.save
+     redirect_to lists_path
+   else
+    render :index
+   end
+  end
+
+  def edit
+   @list = List.find(params[:id])
+  end
+
+  def update
+     @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to list_path(list.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+     @list = List.find(params[:id])
+    if @list.destroy
+       redirect_to lists_path
+    else
+      render :show
+    end
+  end
+
+  private
+   def list_params
+    params.require(:list).permit(:title, :body)
+   end
+
+
+end
